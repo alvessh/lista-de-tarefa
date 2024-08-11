2c443775f0776aa06aeb85454d08df411eec7cb7
alvessh <117163985+alvessh@users.noreply.github.com>
2024-08-11 12:59:44 -0400

    adiciona crud task backend

---
+++ backend/lista-de-tarefa-api/src/main/java/com/listatarefa/api/ListaDeTarefaApiApplication.java
@@ -0,0 +1,13 @@
+package com.listatarefa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTarefaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeTarefaApiApplication.class, args);
+	}
+
+}
